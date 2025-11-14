2c64de1877839abee52f3fd0e13b39934ba26ff9
Alt <144161125+Altonot@users.noreply.github.com>
2025-11-13 21:12:24 -0700

    Gameplay Mechanics
    Worked on placing/breaking blocks, having a specific zone where we can place the blocks.

---
+++ project-avalon-team-vitheria/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make it move based on rotation of y axis of camera
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        
+        transform.Translate(Camera.main.transform.forward * verticalInput * speed * Time.deltaTime);
+        transform.Translate(Camera.main.transform.right * horizontalInput * speed * Time.deltaTime);
+    }
+
+}
---
+++ project-avalon-team-vitheria/Assets/Scripts/BuildSystem.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildSystem : MonoBehaviour
+{
+    public Transform shootingPoint;
+    public GameObject blockObject;
+
+    public Transform parent;
+
+    public Color normalColor;
+    public Color highlightedColor;
+
+    GameObject lastHightlightedBlock;
+
+    public bool ZoneObject = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            BuildBlock(blockObject);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            DestroyBlock();
+        }
+        HighlightBlock();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Zone"))
+        {
+            ZoneObject = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Zone"))
+        {
+            ZoneObject = false;
+        }
+    }
+
+    void BuildBlock(GameObject block)
+    {
+        if(Physics.Raycast(shootingPoint.position, shootingPoint.forward, out RaycastHit hitInfo) && ZoneObject)
+        {
+
+            if(hitInfo.transform.tag == "Block") //Tag to check if we hit a 'block' object
+            {
+                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x + hitInfo.normal.x/2), Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y / 2), Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z /2));
+                Instantiate(block, spawnPosition, Quaternion.identity, parent);
+            }
+            else
+            {
+                Vector3 spawnPosition = new Vector3(Mathf.RoundToInt(hitInfo.point.x), Mathf.RoundToInt(hitInfo.point.y), Mathf.RoundToInt(hitInfo.point.z));
+                Instantiate(block, spawnPosition, Quaternion.identity, parent);
+            }
+        }
+    }
+
+    void DestroyBlock()
+    {
+        if (Physics.Raycast(shootingPoint.position, shootingPoint.forward, out RaycastHit hitInfo))
+        {
+            if (hitInfo.transform.tag == "Block") //Another tag check
+            {
+                Destroy(hitInfo.transform.gameObject);
+            }
+        }
+    }
+
+    void HighlightBlock()
+    {
+        if (Physics.Raycast(shootingPoint.position, shootingPoint.forward, out RaycastHit hitInfo))
+        {
+            if (hitInfo.transform.tag == "Block") //Another tag check
+            {
+                if(lastHightlightedBlock == null)
+                {
+                    lastHightlightedBlock = hitInfo.transform.gameObject;
+                    hitInfo.transform.gameObject.GetComponent<Renderer>().material.color = highlightedColor;
+                }
+                else if (lastHightlightedBlock != hitInfo.transform.gameObject)
+                {
+                    lastHightlightedBlock.GetComponent<Renderer>().material.color = normalColor;
+                    hitInfo.transform.gameObject.GetComponent<Renderer>().material.color = highlightedColor;
+                    lastHightlightedBlock = hitInfo.transform.gameObject;
+                }
+            }
+        }
+    }
+}
