f21a1768920bc2d7dfa25ba1821ba32c884abe0e
Alt <144161125+Altonot@users.noreply.github.com>
2025-11-15 14:49:28 -0700

    Inventory UI 1.0
    Creating framework for inventory system

---
+++ project-avalon-team-vitheria/Assets/Scripts/Sandbox/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory Singleton;
+    public static InventoryItem carriedItem;
+    [SerializeField] InventoryItem itemPrefab;
+
+    [Header("Item List")]
+    [SerializeField] Item[] items;
+    void Awake()
+    {
+        Singleton = this;
+    }
+
+    void Update()
+    {
+        if(carriedItem == null) return;
+
+        carriedItem.transform.position = Input.mousePosition;
+    }
+
+    public void SetCarriedItem(InventoryItem item)
+    {
+        if(carriedItem != null)
+        {
+            if(item.activeSlot != carriedItem.myItem) return;
+            item.activeSlot.SetItem(carriedItem);
+        }
+
+        carriedItem = item;
+        carriedItem.canvasGroup.blocksRaycasts = false;
+        item.transform.SetParent(draggablesTransform);
+    }
+}
