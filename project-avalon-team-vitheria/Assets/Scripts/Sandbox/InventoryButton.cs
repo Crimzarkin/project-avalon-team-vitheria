ba5767acff6a45e2421e808bae405e2c603745a8
Alt <144161125+Altonot@users.noreply.github.com>
2025-11-25 20:52:34 -0700

    UI Fix
    UI now teleports infront of player when opened

--- project-avalon-team-vitheria/Assets/Scripts/Sandbox/InventoryButton.cs
+++ project-avalon-team-vitheria/Assets/Scripts/Sandbox/InventoryButton.cs
@@ -9,6 +9,8 @@ public class InventoryButton : MonoBehaviour
     public BuildSystem buildSystem;
 
     private InputDevice controller;
+    public Transform player;
+    public float distance = 2.0f;
     private bool buttonPressed = false;
     private bool lastButtonPressed = false;
 
@@ -31,12 +33,17 @@ public class InventoryButton : MonoBehaviour
         bool buttonPressed = false;
         // Read touchpad click (primary2DAxisClick)
         if (controller.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out buttonPressed) && buttonPressed && !lastButtonPressed)
-        
-     
         {
-       ;
+
             // Toggle UI
             inventoryUI.SetActive(!inventoryUI.activeSelf);
+
+            if(inventoryUI.activeSelf)
+            {
+                Vector3 newPos = player.position + player.forward * distance;
+                inventoryUI.transform.position = newPos;
+                inventoryUI.transform.rotation = Quaternion.LookRotation(player.forward);
+            }
 
             // Update build system (block placement only when closed)
             buildSystem.inventoryClosed = !inventoryUI.activeSelf;
