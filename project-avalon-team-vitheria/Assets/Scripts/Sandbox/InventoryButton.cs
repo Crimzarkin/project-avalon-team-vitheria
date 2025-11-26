c55bd23f114b5583a9d907d1c1a0b55a303016ac
Alt <144161125+Altonot@users.noreply.github.com>
2025-11-25 21:18:23 -0700

    Fix UI to teleport to player

--- project-avalon-team-vitheria/Assets/Scripts/Sandbox/InventoryButton.cs
+++ project-avalon-team-vitheria/Assets/Scripts/Sandbox/InventoryButton.cs
@@ -10,7 +10,9 @@ public class InventoryButton : MonoBehaviour
 
     private InputDevice controller;
     public Transform player;
-    public float distance = 2.0f;
+    private float uiDistance = 2.0f;
+    private float uiScale = 0.01f;
+    private float uiHeight = 0.5f;
     private bool buttonPressed = false;
     private bool lastButtonPressed = false;
 
@@ -35,15 +37,15 @@ public class InventoryButton : MonoBehaviour
         if (controller.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out buttonPressed) && buttonPressed && !lastButtonPressed)
         {
 
-            // Toggle UI
-            inventoryUI.SetActive(!inventoryUI.activeSelf);
-
             if(inventoryUI.activeSelf)
             {
-                Vector3 newPos = player.position + player.forward * distance;
+                Vector3 newPos = player.position + player.forward * uiDistance + Vector3.up * uiHeight;
                 inventoryUI.transform.position = newPos;
-                inventoryUI.transform.rotation = Quaternion.LookRotation(player.forward);
+                inventoryUI.transform.localScale = new Vector3(uiScale, uiScale, uiScale);
+                inventoryUI.transform.LookAt(player);
             }
+            // Toggle UI
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
 
             // Update build system (block placement only when closed)
             buildSystem.inventoryClosed = !inventoryUI.activeSelf;
