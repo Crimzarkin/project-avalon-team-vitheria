954bcb0edf6e150fba4c42b8fe4314b3e908cf31
Mark Guerta <144377385+Mark-Guerta@users.noreply.github.com>
2026-01-07 16:43:01 -0700

    Implemented treasure location randomizer
    Created treasure spawner script for randomly placing treasure in unique locations.

---
+++ project-avalon-team-vitheria/Assets/TreasureSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TreasureSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] treasurePrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateTreasure(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void generateTreasure(int amount = 5)
+    {
+        int[] randPositions = {};
+        int numOfPositions = spawnPoints.Length;
+        int position;
+        
+        if (amount > numOfPositions)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount),"Amount of objects exceeds available spawn locations");
+        }
+
+        for (int loop = 0; loop < amount; loop++)
+        {
+            position = UnityEngine.Random.Range(0,numOfPositions);
+            while (randPositions.Contains(position))
+            {
+                position = UnityEngine.Random.Range(0,amount);
+            }
+            randPositions.Append(position);
+            Instantiate(treasurePrefabs[0], spawnPoints[position].position, transform.rotation);
+        }
+    }
+}
