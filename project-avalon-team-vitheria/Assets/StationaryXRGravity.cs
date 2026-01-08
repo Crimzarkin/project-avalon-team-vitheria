eaad738dac6f9f987e09fee3a0bd1b7e9ff42994
Crimzarkin <crimzarkin@gmail.com>
2026-01-08 15:47:10 -0700

    added gravity

---
+++ project-avalon-team-vitheria/Assets/StationaryXRGravity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class StationaryXRGravity : MonoBehaviour
+{
+    public float gravity = -9.81f;
+    private CharacterController controller;
+    private float yVelocity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded && yVelocity < 0)
+            yVelocity = -2f;
+
+        yVelocity += gravity * Time.deltaTime;
+        controller.Move(Vector3.up * yVelocity * Time.deltaTime);
+    }
+}
