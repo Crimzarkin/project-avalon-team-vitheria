ce3f4d1373ec4824548f06c88c30fccecb25a635
Alt <144161125+Altonot@users.noreply.github.com>
2026-01-09 20:13:39 -0700

    Adding PostProcessing and Hook

---
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/UnderwaterDepth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class UnderwaterDepth : MonoBehaviour
+{
+    public Transform mainCamera;
+    public int depth = 0;
+    public PostProcessVolume volume;
+    public PostProcessProfile surfaceProfile;
+    public PostProcessProfile underwaterProfile;
+
+    void Update()
+    {
+        if(mainCamera.position.y < depth)
+        {
+            EnableUnderwaterEffects(true);
+        }
+        else
+        {
+            EnableUnderwaterEffects(false);
+        }
+    }
+
+    private void EnableUnderwaterEffects(bool active)
+    {
+        if (active)
+        {
+            volume.profile = underwaterProfile;
+        }
+        else
+        {
+            volume.profile = surfaceProfile;
+        }
+    }
+}
