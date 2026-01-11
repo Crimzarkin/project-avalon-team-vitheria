f611aa15b76f7e38672a24932747ed603a9c5f3a
Mark Guerta <144377385+Mark-Guerta@users.noreply.github.com>
2026-01-11 14:58:47 -0700

    Testing Lever Movement
    Checking if lever goes beyond its expected boundaries. Might implement Hinge Joint component instead.

---
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlJoystick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJoystick : MonoBehaviour
+{
+    public GameObject Joystick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
---
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlLever.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlLever : MonoBehaviour
+{
+    public GameObject Lever;
+    float velocityZ = 0.2f;
+    float posZ;
+    bool forward = true;
+    float upperBound = 0.2f;
+    float lowerBound = -0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        upperBound += Lever.transform.position.z;
+        lowerBound += Lever.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        leverDisplacement();
+    }
+    void leverDisplacement()
+    {
+        if( (forward && Lever.transform.position.z >= upperBound) || (!forward && Lever.transform.position.z <= lowerBound))
+        {
+            forward = !forward;
+            Debug.Log("Hit");
+        }
+
+        if (forward)
+        {
+            Lever.transform.Translate(Vector3.forward * Time.deltaTime * velocityZ);
+        }
+        else
+        {
+            Lever.transform.Translate(Vector3.forward * Time.deltaTime * -velocityZ);
+        }
+        
+    }
+}
