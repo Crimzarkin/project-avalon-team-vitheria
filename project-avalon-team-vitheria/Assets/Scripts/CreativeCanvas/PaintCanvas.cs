7a9ab623a5071c085217f42cd0515d11b44a1030
Crimzarkin <crimzarkin@gmail.com>
2026-01-14 21:05:13 -0700

    added painting scripts for canvas game

---
+++ project-avalon-team-vitheria/Assets/Scripts/CreativeCanvas/PaintCanvas.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintCanvas : MonoBehaviour
+{
+    public int textureSize = 1024;
+    public int brushSize = 6;
+
+    private Texture2D texture;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        texture = new Texture2D(textureSize, textureSize, TextureFormat.RGBA32, false);
+        texture.filterMode = FilterMode.Point;
+
+        Color[] fill = new Color[textureSize * textureSize];
+        for (int i = 0; i < fill.Length; i++)
+            fill[i] = Color.white;
+
+        texture.SetPixels(fill);
+        texture.Apply();
+
+        rend.material = new Material(Shader.Find("Unlit/Texture"));
+        rend.material.mainTexture = texture;
+    }
+
+    public void Paint(Vector3 hitPoint, Color color)
+    {
+        Vector2 uv = GetUV(hitPoint);
+
+        int x = (int)(uv.x * textureSize);
+        int y = (int)(uv.y * textureSize);
+
+        for (int i = -brushSize; i <= brushSize; i++)
+        {
+            for (int j = -brushSize; j <= brushSize; j++)
+            {
+                texture.SetPixel(x + i, y + j, color);
+            }
+        }
+
+        texture.Apply();
+    }
+
+    Vector2 GetUV(Vector3 hitPoint)
+    {
+        Vector3 local = transform.InverseTransformPoint(hitPoint);
+        return new Vector2(local.x + 0.5f, local.y + 0.5f);
+    }
+}
---
+++ project-avalon-team-vitheria/Assets/Scripts/CreativeCanvas/RightHandPaintInteractor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RightHandPaintInteractor : MonoBehaviour
+{
+    public XRRayInteractor rayInteractor;
+
+    private InputDevice controller;
+    private bool triggerPressed;
+    private bool lastTriggerState;
+
+    public static Color selectedColor = Color.black;
+
+    void Start()
+    {
+        controller = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+    }
+
+    void Update()
+    {
+        if (!controller.isValid)
+            controller = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+        controller.TryGetFeatureValue(CommonUsages.triggerButton, out triggerPressed);
+
+        RaycastHit hit;
+        if (!rayInteractor.GetCurrentRaycastHit(out hit))
+        {
+            lastTriggerState = triggerPressed;
+            return;
+        }
+
+        // If Trigger JUST pressed then select colour
+        if (triggerPressed && !lastTriggerState)
+        {
+            ColorObject colorObj = hit.collider.GetComponent<ColorObject>();
+            if (colorObj != null)
+            {
+                selectedColor = colorObj.color;
+            }
+        }
+
+        // If Trigger HELD then paint
+        if (triggerPressed)
+        {
+            PaintCanvas canvas = hit.collider.GetComponent<PaintCanvas>();
+            if (canvas != null)
+            {
+                canvas.Paint(hit.point, selectedColor);
+            }
+        }
+
+        lastTriggerState = triggerPressed;
+    }
+}
