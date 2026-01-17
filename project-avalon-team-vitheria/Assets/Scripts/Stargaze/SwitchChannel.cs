9c83df8e77933aba8eb456fac305fc56f957d2d8
Alt <144161125+Altonot@users.noreply.github.com>
2026-01-17 15:12:09 -0700

    Remote Added
    Added functionality for clicking remote, changing screens WIP

---
+++ project-avalon-team-vitheria/Assets/Scripts/Stargaze/SwitchChannel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchChannel : MonoBehaviour
+{
+    public GameObject channel1;
+    public GameObject channel2;
+
+    private bool isChannel1Active = true;
+
+    void Start()
+    {
+        ActivateChannel1();
+    }
+
+    void ToggleChannels()
+    {
+        if (isChannel1Active)
+        {
+            ActivateChannel2();
+        }
+        else
+        {
+            ActivateChannel1();
+        }
+    }
+
+    void ActivateChannel1()
+    {
+        channel1.SetActive(true);
+        channel2.SetActive(false);
+        isChannel1Active = true;
+    }
+
+    void ActivateChannel2()
+    {
+        channel1.SetActive(false);
+        channel2.SetActive(true);
+        isChannel1Active = false;
+    }
+}
