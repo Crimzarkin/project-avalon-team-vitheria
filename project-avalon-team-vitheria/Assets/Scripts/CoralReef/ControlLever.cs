fc350fa001ca19644b48c0286ed20adc0729e5ba
Mark Guerta <144377385+Mark-Guerta@users.noreply.github.com>
2026-01-19 14:14:47 -0700

    Transferred movement scripts submarine object

--- project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlLever.cs
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlLever.cs
@@ -7,31 +7,24 @@ public class ControlLever : MonoBehaviour
 {
     public GameObject Lever;
     public GameObject Hand;
-    public GameObject Player;
     // Negative X rotation is up and postive x rotation is down
     public float xDeadzone = 20;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void LateUpdate()
     {
-        throttle();Player.transform.Translate(Vector3.down*Time.deltaTime*5);
+        controlDepth();
     }
-    void throttle()
+
+    void controlDepth()
     {
         float handXRotation = Hand.transform.rotation.x;
         if (handXRotation < -xDeadzone)
         {
-            Player.transform.Translate(Vector3.up*Time.deltaTime*5);
+            transform.Translate(Vector3.up*Time.deltaTime*5);
         }
         else if (handXRotation > xDeadzone)
         {
-            Player.transform.Translate(Vector3.down*Time.deltaTime*5);
+            transform.Translate(Vector3.down*Time.deltaTime*5);
         }
     }
 }
