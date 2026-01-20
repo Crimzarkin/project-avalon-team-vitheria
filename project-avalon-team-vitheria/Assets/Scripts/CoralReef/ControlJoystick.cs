27b803f58a67aa74d8d1e3f18c9ec0984a112eb9
Mark Guerta <144377385+Mark-Guerta@users.noreply.github.com>
2026-01-19 17:03:47 -0700

    Depth Control Behaviour Implemented
    Tilting the controller up and down causes player to change depth

--- project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlJoystick.cs
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlJoystick.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ControlJoystick : MonoBehaviour
 {
     public GameObject Joystick;
+    public GameObject Hand;
     // Start is called before the first frame update
     void Start()
     {
--- project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlLever.cs
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlLever.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Annotations;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ControlLever : MonoBehaviour
 {
-    public GameObject Lever;
     public GameObject Hand;
     // Negative X rotation is up and postive x rotation is down
     public float xDeadzone = 20;
@@ -17,12 +15,12 @@ public class ControlLever : MonoBehaviour
 
     void controlDepth()
     {
-        float handXRotation = Hand.transform.rotation.x;
-        if (handXRotation < -xDeadzone)
+        float handXRotation = Hand.transform.rotation.eulerAngles.x;
+        if ( 180 < handXRotation && handXRotation < 360-xDeadzone)
         {
             transform.Translate(Vector3.up*Time.deltaTime*5);
         }
-        else if (handXRotation > xDeadzone)
+        else if (180 > handXRotation && handXRotation > xDeadzone)
         {
             transform.Translate(Vector3.down*Time.deltaTime*5);
         }
