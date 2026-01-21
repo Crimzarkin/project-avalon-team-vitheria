5c88745a1d6219c45e8349feeb73144722d7153f
Mark Guerta <144377385+Mark-Guerta@users.noreply.github.com>
2026-01-20 19:25:44 -0700

    Forward and backward movement completed

--- project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlJoystick.cs
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlJoystick.cs
@@ -1,45 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ControlJoystick : MonoBehaviour
 {
-    public GameObject Joystick;
-    public GameObject Hand;
+    [SerializeField] private GameObject Joystick;
+    [SerializeField] private GameObject Hand;
     // Negative X rotation is up and postive X rotation is down
-    public float xDeadzone = 20;
+    [SerializeField] private float xDeadzone = 20;
     // Negative Y rotation is left and postive Y rotation is right
-    public float yDeadzone = 20;
-
+    [SerializeField] private float yDeadzone = 20;
+    [SerializeField] private float movementSpeed = 300;
+    [SerializeField] private float rotationSpeed = 500; 
     void LateUpdate()
     {
         controlRotation();
         controlMovement();
     }
 
-    void controlMovement()
+    private void controlMovement()
     {
         float handXRotation = Hand.transform.rotation.eulerAngles.x;
-        if ( 180 < handXRotation && handXRotation < 360-xDeadzone)
+        if (180 < handXRotation && handXRotation < 360 - xDeadzone)
         {
-            transform.Translate(Vector3.up*Time.deltaTime*5);
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
         }
         else if (180 > handXRotation && handXRotation > xDeadzone)
         {
-            transform.Translate(Vector3.down*Time.deltaTime*5);
+            transform.Translate(Vector3.back * Time.deltaTime * movementSpeed);
         }
     }
     
     void controlRotation()
     {
-        float handYRotation = Hand.transform.rotation.eulerAngles.y;
-        if ( 180 < handYRotation && handYRotation < 360-xDeadzone)
+        float handYRotation =  Hand.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y;
+        Debug.Log(handYRotation);
+        if (180 < handYRotation && handYRotation < 360 - yDeadzone)
         {
-
+            transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed);
         }
-        else if (180 > handYRotation && handYRotation > xDeadzone)
+        else if (180 > handYRotation && handYRotation > yDeadzone)
         {
-            
+            transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
         }        
     }
 }
