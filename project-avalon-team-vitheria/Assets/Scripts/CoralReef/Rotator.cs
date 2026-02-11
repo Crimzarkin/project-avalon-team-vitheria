f5942c4a8266f208f5af128e57f1e6a90689c460
Nark <mark.jimenez@ucalgary.ca>
2026-02-11 15:31:03 -0700

    Added TreasureItem Rotation

---
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Editable speed in the Inspector
+    [SerializeField] private float rotationSpeed = 100f;
+
+    void Update()
+    {
+        // Rotates the object around the Y axis (Green axis)
+        // Time.deltaTime ensures it spins smoothly at the same speed on all computers
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
