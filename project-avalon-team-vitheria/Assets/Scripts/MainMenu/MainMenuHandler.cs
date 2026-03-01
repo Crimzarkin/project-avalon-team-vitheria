2e1111df06d534b4322c7dd272879751647b2b41
Alt <144161125+Altonot@users.noreply.github.com>
2026-03-01 13:18:31 -0700

    Update MainMenu Loading
    - Added Loading Screen

--- project-avalon-team-vitheria/Assets/Scripts/MainMenu/MainMenuHandler.cs
+++ project-avalon-team-vitheria/Assets/Scripts/MainMenu/MainMenuHandler.cs
@@ -3,15 +3,22 @@ using UnityEngine.UI;
 using UnityEngine.XR;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 public class MainMenuHandler : MonoBehaviour
 {
     public XRNode controllerNode = XRNode.RightHand;
     private InputDevice controller;
+    private Canvas menuCanvas = null;
+    private Canvas loadingCanvas = null;
+
 
     void Start()
     {
         controller = InputDevices.GetDeviceAtXRNode(controllerNode);
+        menuCanvas = GameObject.Find("Menu").GetComponent<Canvas>();
+        loadingCanvas = GameObject.Find("Loading").GetComponent<Canvas>();
+        loadingCanvas.enabled = false;
     }
         
     public void StartGame(string sceneName)
@@ -21,6 +28,8 @@ public class MainMenuHandler : MonoBehaviour
             Application.Quit();
             return;
         }
-        SceneManager.LoadScene(sceneName);
+        menuCanvas.enabled = false;
+        loadingCanvas.enabled = true;
+        SceneManager.LoadSceneAsync(sceneName);
     }
 }
