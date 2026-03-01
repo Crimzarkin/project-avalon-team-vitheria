ab0557b49f03f908388a14265fe1cea1ea7a3bc6
Alt <144161125+Altonot@users.noreply.github.com>
2026-02-28 22:06:55 -0700

    Merge Ready Sandbox

--- project-avalon-team-vitheria/Assets/Scripts/LucisGarden/SeedItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SeedItem : MonoBehaviour
-{
-    public Material seedMaterial;
-    public GameObject plantPrefab;
-}
