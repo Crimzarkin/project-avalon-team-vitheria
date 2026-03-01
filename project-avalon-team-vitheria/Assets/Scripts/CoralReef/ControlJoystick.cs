bb179af4cb0452b9d4cccadf40bccad18cf7abd5
Mark Guerta <144377385+Mark-Guerta@users.noreply.github.com>
2026-03-01 11:18:00 -0700

    Imported Coral Reef Scene

---
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlJoystick.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class ControlJoystick : MonoBehaviour
+{
+    [SerializeField] private GameObject Joystick;
+    [SerializeField] private GameObject Hand;
+    // Negative X rotation is up and postive X rotation is down
+    [SerializeField] private float xDeadzone = 10;
+    // Negative Y rotation is left and postive Y rotation is right
+    [SerializeField] private float yDeadzone = 10;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 15f; 
+    private Rigidbody playerRB;
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+    void LateUpdate()
+    {
+        controlRotation();
+        controlMovement();
+    }
+
+    private void controlMovement()
+    {
+        float handXRotation = Hand.transform.localEulerAngles.x;
+        if (180 < handXRotation && handXRotation < 360 - xDeadzone)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+        }
+        else if (180 > handXRotation && handXRotation > xDeadzone)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * movementSpeed);
+        }
+    }
+    
+    private void controlRotation()
+    {
+        float handYRotation = Hand.transform.localEulerAngles.y;
+        if (yDeadzone < handYRotation && handYRotation < 180)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+        }
+        else if (180 < handYRotation && handYRotation < 360 - yDeadzone)
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed);
+        }        
+    }
+}
---
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/ControlLever.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ControlLever : MonoBehaviour
+{
+    [SerializeField] private GameObject Lever;
+    [SerializeField] private GameObject Hand;
+    [SerializeField] private float verticalSpeed = 5f;
+    private Rigidbody playerRB;
+    // Negative X rotation is up and postive x rotation is down
+    public float xDeadzone = 10;
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+    void LateUpdate()
+    {
+        controlDepth();
+    }
+
+    void controlDepth()
+    {
+        float handXRotation = Hand.transform.localEulerAngles.x;
+        if ( 180 < handXRotation && handXRotation < 360-xDeadzone)
+        {
+            // playerRB.AddForce(transform.up * verticalSpeed);
+            transform.Translate(Vector3.up * Time.deltaTime * verticalSpeed);
+        }
+        else if (180 > handXRotation && handXRotation > xDeadzone)
+        {
+            // playerRB.AddForce(transform.up * -verticalSpeed);
+            transform.Translate(Vector3.down * Time.deltaTime * verticalSpeed);
+        }
+    }
+}
