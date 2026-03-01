1de0a19254e4c91c72417bd3720924e0066fadcc
Alt <144161125+Altonot@users.noreply.github.com>
2026-02-28 22:17:48 -0700

    Revert "Merge branch 'Sandbox' into Working-Main"
    This reverts commit 785f180082c0e3b0a850f9238a36550a37b68a7e, reversing
    changes made to a5d0c887896cd469392c9a23c418eef5ea5d0c28.

---
+++ project-avalon-team-vitheria/Assets/Scripts/LucisGarden/SeedItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedItem : MonoBehaviour
+{
+    public Material seedMaterial;
+    public GameObject plantPrefab;
+}
