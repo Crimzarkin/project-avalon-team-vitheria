255e6d986199eead412c539830411f1eb57a0acd
Crimzarkin <crimzarkin@gmail.com>
2026-03-07 12:18:06 -0700

    Added main menu fade in and soundtrack. Fixed beach house ost

---
+++ project-avalon-team-vitheria/Assets/CanvasScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScript : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public float fadeSpeed = 0f;
+
+    void Start()
+    {
+        canvasGroup.alpha = 0f;
+    }
+
+    void Update()
+    {
+        if (canvasGroup.alpha < 1f)
+        {
+            canvasGroup.alpha += Time.deltaTime * fadeSpeed;
+        }
+    }
+}
