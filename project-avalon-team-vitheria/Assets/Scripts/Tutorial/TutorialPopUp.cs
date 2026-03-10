ccf84540e831462c927460913f9a803da1f1202b
The Chosen One <ivan.ibe@ucalgary.ca>
2026-03-09 18:12:24 -0600

    Added Tutorial Pop up script

---
+++ project-avalon-team-vitheria/Assets/Scripts/Tutorial/TutorialPopUp.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TutorialPopUpHandler : MonoBehaviour
+{
+
+    public XRNode controllerNode = XRNode.RightHand;
+    public Button toggleButton;
+    public GameObject tutorialScreen;
+
+    void Start()
+    {
+
+    }
+}
