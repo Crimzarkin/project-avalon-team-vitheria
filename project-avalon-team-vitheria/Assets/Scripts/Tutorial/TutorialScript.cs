327c581d90f43d17af41afc3da0900fe4ef80e8f
Nark <mark.jimenez@ucalgary.ca>
2026-03-09 19:49:06 -0600

    test #1 FAILED

---
+++ project-avalon-team-vitheria/Assets/Scripts/Tutorial/TutorialScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TutorialPopUpHandler : MonoBehaviour
+{
+    public Transform player;
+    public XRNode controllerNode = XRNode.RightHand;
+    public Button toggleButton;
+    public GameObject tutorialScreen;
+    private float TuiDistance = 4.0f;
+    private float TuiScale = 0.01f;
+    private float TuiHeight = 1.5f;
+
+    void Start()
+    {
+        tutorialScreen.SetActive(true);
+    }
+    void Update() {
+        Vector3 newPos = player.position + player.forward * TuiDistance + Vector3.up * TuiHeight;
+                tutorialScreen.transform.position = newPos;
+                tutorialScreen.transform.localScale = new Vector3(TuiScale, TuiScale, TuiScale);
+                tutorialScreen.transform.LookAt(player);
+      
+    }
+    
+}
