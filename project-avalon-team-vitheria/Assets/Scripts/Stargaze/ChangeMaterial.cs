83d908ef52128de770671aa45160026961d145f6
someone735 <smeone83@gmail.com>
2026-03-11 22:36:25 -0600

    Repeat - importing stargazing

---
+++ project-avalon-team-vitheria/Assets/Scripts/Stargaze/ChangeMaterial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    public Material newMaterial;
+    public Material oldMaterial;
+
+    public void updateMaterial()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null && newMaterial != null)
+        {
+            renderer.material = newMaterial;
+        }
+    }
+
+    public void returnMaterial()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null && oldMaterial != null)
+        {
+            renderer.material = oldMaterial;
+        }
+    }
+}
---
+++ project-avalon-team-vitheria/Assets/Scripts/Stargaze/SwitchChannel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchChannel : MonoBehaviour
+{
+    public List<Sprite> channels = new List<Sprite>();
+    public GameObject map;
+    private int channel = 1;
+
+    public void ToggleChannels()
+    {
+        if (channel < channels.Count)
+        {
+            map.GetComponent<SpriteRenderer>().sprite = channels[channel];
+            channel++;
+        }
+        else
+        {
+            map.GetComponent<SpriteRenderer>().sprite = channels[0];
+            channel = 1;
+        }
+    }
+}
