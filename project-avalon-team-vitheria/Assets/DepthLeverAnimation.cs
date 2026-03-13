b0b8a0596298415601ba9db30988631019bdb4ff
Mark Guerta <144377385+Mark-Guerta@users.noreply.github.com>
2026-03-12 22:21:07 -0600

    Created animations for depth lever

---
+++ project-avalon-team-vitheria/Assets/DepthLeverAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthLeverAnimation : MonoBehaviour
+{
+
+    bool Forward;
+    bool Reverse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- project-avalon-team-vitheria/Assets/Scripts/CoralReef/TreasureItem.cs
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/TreasureItem.cs
@@ -3,19 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TreasureItem : MonoBehaviour
-
-
 {
     [SerializeField] private AudioClip pickupSound;
-    
     void OnCollisionEnter(Collision collision)
     {
-        
         if (collision.gameObject.CompareTag("Player"))
         {
-          
-        
-        
             AudioSource.PlayClipAtPoint(pickupSound, transform.position);
             Destroy(gameObject);
         }
