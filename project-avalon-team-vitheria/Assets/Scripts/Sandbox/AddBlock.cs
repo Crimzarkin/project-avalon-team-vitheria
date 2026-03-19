b83ba01216056f67cce1ebf480c5d178006bfd29
Alt <144161125+Altonot@users.noreply.github.com>
2026-03-18 19:01:14 -0600

    Sandbox Update
     - Fix Chests, Button Models
    - Add audio clips to chests

--- project-avalon-team-vitheria/Assets/Scripts/Sandbox/AddBlock.cs
+++ project-avalon-team-vitheria/Assets/Scripts/Sandbox/AddBlock.cs
@@ -7,12 +7,26 @@ public class AddBlock : MonoBehaviour
     public GameObject inventoryObject;
     public Item item;
     public bool addedToInventory = false;
+    public AudioSource newItemSound;
+    public AudioSource oldItemSound;
     public void AddBlockToInventory()
     {
         if (inventoryObject != null && addedToInventory == false)
         {
             inventoryObject.GetComponent<Inventory>().AddItem(item);
             addedToInventory = true;
+        }
+    }
+
+    public void PlaySound()
+    {
+        if(addedToInventory == false)
+        {
+            newItemSound.Play();
+        }
+        else
+        {
+            oldItemSound.Play();
         }
     }
 }
