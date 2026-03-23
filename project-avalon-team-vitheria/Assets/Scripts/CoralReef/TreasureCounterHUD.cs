15bf775391374f761a5e810648002076fb236320
Nark <mark.jimenez@ucalgary.ca>
2026-03-23 00:33:24 -0600

    Revert "Merge branch 'Coral-Reef-UI' into Working-Main"
    This reverts commit b1f10e789735621c3136d1401aa5449421db27f3, reversing
    changes made to c26603b33340986ca9ac9e275d351ba4dba9270b.

--- project-avalon-team-vitheria/Assets/Scripts/CoralReef/TreasureCounterHUD.cs
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/TreasureCounterHUD.cs
@@ -1,39 +1,33 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TreasureCounterHUD : MonoBehaviour
 {
+    private GameObject[] treasure;
     private int count;
     [SerializeField] private GameObject hudText;
-    private TextMeshProUGUI counterDisplay;
     // Start is called before the first frame update
     void Start()
     {
-        counterDisplay = hudText.GetComponent<TextMeshProUGUI>();
+
     }
 
     // Update is called once per frame
 
     public void decreaseCounter()
     {
-        if (count > 0)
-        {
-            count -= 1;
-            counterDisplay.text = "Remaining treasure: " + count.ToString();            
-        }
-        else
-        {
-            counterDisplay.text = "All treasure has been found! Return to main menu or reset the game.";
-        }
+        count -= 1;
+        Text counterText = hudText.GetComponent<Text>();
+        counterText.text = "Remaining treasure: " + count.ToString();
     }
 
     public void setcounterText(int number)
     {
         count = number;
-        counterDisplay.text = "Remaining treasure: " + number.ToString();
+        Text counterText = hudText.GetComponent<Text>();
+        counterText.text = "Remaining treasure: " + number.ToString();
     }
 }
--- project-avalon-team-vitheria/Assets/Scripts/CoralReef/TreasureSpawner.cs
+++ project-avalon-team-vitheria/Assets/Scripts/CoralReef/TreasureSpawner.cs
@@ -35,9 +35,9 @@ public class TreasureSpawner : MonoBehaviour
 
         if (amount > numOfPositions)
         {
-            throw new ArgumentOutOfRangeException(nameof(amount),"Amount of treasure exceeds available spawn locations: " + numOfPositions);
+            throw new ArgumentOutOfRangeException(nameof(amount),"Amount of treasure exceeds available spawn locations");
         }
-        GameObject folder = new GameObject("Coins");
+
         for (int loop = 0; loop < amount; loop++)
         {
             do
@@ -46,9 +46,8 @@ public class TreasureSpawner : MonoBehaviour
                 treasurePrefabSelector = Random.Range(0,treasurePrefabs.Length);
             } while (randPositions.Contains(treasurePosition));
             randPositions[loop] = treasurePosition;
-            Instantiate(treasurePrefabs[treasurePrefabSelector], spawnPoints[treasurePosition].position, transform.rotation, folder.transform);
+            Instantiate(treasurePrefabs[treasurePrefabSelector], spawnPoints[treasurePosition].position, transform.rotation);
         }
-        GameObject.Find("Player").GetComponent<TreasureCounterHUD>().setcounterText(amount);
     }
 
     // Fetches all tagged spawnPoints in the scene
@@ -60,5 +59,6 @@ public class TreasureSpawner : MonoBehaviour
         {
             spawnPoints[spawnPoint] = spawnPointObjects[spawnPoint].transform;
         }
+        GameObject.Find("HUD").GetComponent<TreasureCounterHUD>().setcounterText(treasureAmount);
     }
 }
